19a907656dd68efe1f528d31cb13c098d09cfe60
ROSHAN RAJPUT <rajputrosh780@gmail.com>
2023-12-29 07:18:00 +0000

    changes

---
+++ 01_Basics/conversion.js
@@ -0,0 +1,16 @@
+// let a = 9;
+
+// let change= String(a);
+// console.log(typeof(change));
+ 
+
+// let char ="hello";
+
+// let changes = Number(char);
+// console.log(typeof(changes));
+
+
+// let b = "js";
+
+// let Bchange =Boolean(b);
+// console.log(Bchange);
