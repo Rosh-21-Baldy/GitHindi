c94ca3b45b655eb420abe8e358b64d88b1b7843a
ROSHAN RAJPUT <rajputrosh780@gmail.com>
2023-12-29 05:38:03 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,26 @@
+// use of let is to declare a variable 
+
+// let age = 18;
+
+// let name = "you";
+
+// let boolean = true / false (0,1);
+
+// null => standalone value
+
+// undefined    
+
+// symbol => unique
+
+
+//  typeof() is used to find the datatype 
+
+// console.log(typeof(18)); 
+
+// console.log(typeof("you")); 
+
+// console.log(typeof(null)); 
+            
+// output is object some say its a language mistake but null actually an object type;
+
+// console.log(typeof(undefined));
