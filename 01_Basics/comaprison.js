a7c05c2936647cac67c0b9f19504f1a9e942501f
ROSHAN RAJPUT <rajputrosh780@gmail.com>
2023-12-30 17:45:46 +0000

    cpmaprison config

---
+++ 01_Basics/comaprison.js
@@ -0,0 +1,40 @@
+
+// str1 = "Git"
+
+// str2 ="hub"
+
+// console.log(str1+str2);
+
+
+// let a = 45;
+
+// let b = 77;
+ 
+// console.log(a-b);
+
+
+// let a = 6;
+// let b = 5;
+
+// b = ++a;
+// console.log(b);
+// console.log(a);
+
+// let a =6;
+// let b= 5;
+//  b=a++;
+//  console.log(b);
+//  console.log(a);
+
+//  always give the result in boolean 
+
+// console.log("4"==4);
+// console.log(4===4);
+
+// console.log("h">1); 
+// console.log(null>0);
+
+// === strict check 
+// checks the value as well as datatypes :
+
+// console.log(1==="1");
