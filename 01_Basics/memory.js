fd60478d45a8ec75a45c76121e649aad231ac095
ROSHAN RAJPUT <rajputrosh780@gmail.com>
2023-12-31 09:32:38 +0000

    memory config

---
+++ 01_Basics/memory.js
@@ -0,0 +1,26 @@
+// stack (primitive)  heap (non-primitive)
+
+// let name = "ron"
+//  let title = "kumar"
+
+// name = title;
+// console.log(name);
+// console.log(title);
+
+//  non primitive
+// to Dot(.) access the value
+// let user_1={
+
+//     email :"to@gmail.com",
+//     upiID :"bank@oksbi"
+
+// }
+//  let user_2 = user_1
+    
+//  user_2.email ="ronkumar@gmail.com";
+
+//  console.table(user_2);
+//  console.table(user_1);
+
+
+
