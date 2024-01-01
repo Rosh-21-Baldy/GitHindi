dee03342162d54bc3a9059346d84dc7363c8fa7c
ROSHAN RAJPUT <rajputrosh780@gmail.com>
2024-01-01 18:42:35 +0000

    add array

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,31 @@
+// const marvel_heros =["bhais","chor","thans"];
+
+// const dc_heros =["chamkila","kacha","bijli"];
+
+// dc_heros.push(marvel_heros);
+// console.log(dc_heros);
+
+// const allheros = dc_heros.concat(marvel_heros);
+// console.log(allheros);
+
+ /////////////
+//  best way to add String(array)
+// const all_new =[...marvel_heros,...dc_heros];
+// console.log(all_new);
+
+
+// let att =["hello","hi","bye"];
+
+// let rtt =["no","yes","nan"];
+
+// let addarr =[...att,...rtt];
+
+// console.log(addarr);
+//////////////////////////////
+// if condition occur array in multiple array 
+
+// const arr =[1,34,[45,67,78],87,[34,12,67,98],546,67,[324,768],6];
+
+// const real_array = arr.flat(Infinity);
+
+// console.table(real_array);
