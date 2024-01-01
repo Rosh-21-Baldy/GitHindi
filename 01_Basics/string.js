78d446b2f07fd3bad39c1425225706f71ad6c240
ROSHAN RAJPUT <rajputrosh780@gmail.com>
2024-01-01 14:44:51 +0000

    stringconfig

---
+++ 01_Basics/string.js
@@ -0,0 +1,15 @@
+// const name = "ram"
+//  const title = "kumar"
+
+// console.log(name + title);
+ 
+// ^^^^^^ ^^^^^^ ^^^^^^^^^^ little bit older way to print string
+
+// ++++++++++++++++++++++++++++++++++++
+// `` -> backticks 
+//   const name = "ram"
+//   const info = "CSE"
+// console.log(`hello my name is ${name} currently i am a btech student in Ist year ${info}`)
+ 
+ 
+
