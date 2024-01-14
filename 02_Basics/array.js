bd2acfbf31646a77bfc812bca2acf9ea0640617b
ROSHAN RAJPUT <rajputrosh780@gmail.com>
2024-01-14 16:21:49 +0000

    function and map basics

---
+++ 02_Basics/array.js
@@ -0,0 +1,32 @@
+// const arr =[1,35,7,84,5]
+// console.table(arr);
+
+
+// const char = ['a','b','c','d'];
+// console.table(char);
+
+// console.log(char[0]);
+
+//////////////////////////////////////
+
+// const rar =[1,5,"hlo","hi",78];
+
+// console.table(rar);
+
+// rar.push(9);
+// rar.pop(); -> it will automatically delete the element at last 
+// console.log(rar);
+
+///////////////////////////////// 
+
+// const mar =[1,2,3,4];
+// mar.unshift(10) ->   add a value at first position
+
+// mar.shift();-> del the first element
+
+// console.log(mar);    
+
+
+
+
+
